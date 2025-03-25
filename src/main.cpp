b1b5ec1cff828f465a35ef818ac411f1b68c5555
WangManhe <2775526580@qq.com>
2025-03-25 23:32:28 +0800

    初步学习

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
-    app.setStyle("windows11");
+    app.setStyle("windows10");
     MainWindow window;
     window.show();
     return app.exec();
